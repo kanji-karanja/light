44f5634b264afd876c90882c1724e8f9e5de2e78
Karim K. Kanji <karimkanji101@gmail.com>
2019-05-10 10:55:48 +0300

    Updated gearhost credentials

--- assets/serve/credentials.php
+++ assets/serve/credentials.php
@@ -1,3 +1,4 @@
 <?php
-$conn= new mysqli("localhost","root","","light");
+//$conn= new mysqli("localhost","root","","light");
+$conn= new mysqli("den1.mysql4.gear.host","lightke","Ad177J~i74Y!","lightke");
 ?>
